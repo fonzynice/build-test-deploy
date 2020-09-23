7ec682b858a3293d9c6bdfc89b0c9cce8ac55d42
fonzy <fonzynice@gmail.com>
2020-09-23 07:22:06 -0400

    first push

---
+++ testing.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
+import pprint
+
+nr = InitNornir(config_file="config.yaml")
+r2 = nr.run(
+    task=netmiko_send_command,
+    command_string="show ip route"
+)
+
+r = nr.run(
+    task=napalm_get,
+    getters=["facts"]
+)
+pprint.pprint(r2["rtr1"][0].result)
